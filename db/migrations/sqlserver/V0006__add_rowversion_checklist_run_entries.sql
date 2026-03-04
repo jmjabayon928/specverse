26834c1a4f42e39525c9421d45151caa06559eb7
Jeff Abayon <jmjabayon@gmail.com>
2026-03-04 09:02:11 -0700

    chore(db): add rowversion to checklist run entries and update gitignore

--- db/migrations/sqlserver/V0006__add_rowversion_checklist_run_entries.sql
+++ db/migrations/sqlserver/V0006__add_rowversion_checklist_run_entries.sql
@@ -1,4 +1,4 @@
-﻿IF COL_LENGTH('dbo.ChecklistRunEntries', 'RowVersion') IS NULL
+IF COL_LENGTH('dbo.ChecklistRunEntries', 'RowVersion') IS NULL
 BEGIN
   ALTER TABLE dbo.ChecklistRunEntries
   ADD RowVersion rowversion NOT NULL;
